beed7a513f1a4d054ee065077cbef4351c4a0f05
rayfanaqbil <rayfana09@gmail.com>
2023-10-25 21:01:36 +0700

    Praktikum 5

---
+++ Pertemuan05/Praktikum/P5_1_714220044/P5_1_714220044/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace P5_1_714220044
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OutputLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.HelloButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // OutputLabel
+            // 
+            this.OutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.OutputLabel.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OutputLabel.Location = new System.Drawing.Point(5, 20);
+            this.OutputLabel.Name = "OutputLabel";
+            this.OutputLabel.Size = new System.Drawing.Size(264, 23);
+            this.OutputLabel.TabIndex = 0;
+            this.OutputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 1;
+            // 
+            // HelloButton
+            // 
+            this.HelloButton.Location = new System.Drawing.Point(57, 87);
+            this.HelloButton.Name = "HelloButton";
+            this.HelloButton.Size = new System.Drawing.Size(75, 25);
+            this.HelloButton.TabIndex = 2;
+            this.HelloButton.Text = " &Say Hello";
+            this.HelloButton.UseVisualStyleBackColor = true;
+            this.HelloButton.Click += new System.EventHandler(this.HelloButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(161, 87);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 25);
+            this.ExitButton.TabIndex = 3;
+            this.ExitButton.Text = "E&xit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(284, 136);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.HelloButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.OutputLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label OutputLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button HelloButton;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
---
+++ Pertemuan05/Praktikum/P5_3_714220044/P5_3_714220044/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P5_3_714220044
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTampilkan_Click(object sender, EventArgs e)
+        {
+            string os = "";
+            string status = "Ya sudah diperbaiki";
+
+            if (rb_android.Checked == true)
+            {
+                os = "Android";
+            }
+            else if (rb_ios.Checked == true)
+            {
+                os = "iOS";
+            }
+
+            MessageBox.Show(
+                "Merk HP: " + txtMerkHP.Text +
+                "\nSistem Operasi : " + os  +
+                "\nStatus Perbaikan : " + status,
+                "Informasi Service HP",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void textMerkHP_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnKeluar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
