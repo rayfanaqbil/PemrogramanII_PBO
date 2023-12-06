3644dada2cc8539cf26a6bc0965841379f935a6a
rayfanaqbil <rayfana09@gmail.com>
2023-12-06 15:18:41 +0700

    pertemuan 10

---
+++ Pertemuan10/Praktikum/P10_714220044/P10_714220044/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P10_714220044
+{
+    public partial class Form1 : Form
+    {
+        Koneksi koneksi = new Koneksi();
+
+        public void Tampil()
+        {
+            //Query DB MGA
+            DataMahasiswa.DataSource = koneksi.ShowData("SELECT * FROM t_mahasiswa ");
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Memanggil method tampil
+            Tampil();
+        }
+    }
+}
